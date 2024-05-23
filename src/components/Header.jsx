b578ab28e4be6c32369fc91e66ed1c6581248c13
Retno Ardanari <retnoardanari1728@gmail.com>
2024-05-23 22:32:42 +0700

    fuzzy tsukamoto pertanian

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <>
+            <nav className="">
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
